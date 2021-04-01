701d7a45d5505d67a7e46367ab01234743f889b6
samharshe <samharshe29@gmail.com>
2021-04-01 18:39:45 -0400

    added see more photos functionality and patches some bugz

--- js/app.js
+++ js/app.js
@@ -1,60 +1,28 @@
-var row_to_show = 3
-var num_rows = 17;
-var num_pics = 68;
-var show_more_button = document.getElementById("show_more_button");
+const showNextRow = function(){
+    let mediaRow = document.createElement('section');
+    mediaRow.classList.add('mediaRow');
 
-show_more_button.addEventListener("click", function(e){
-    row_to_show++;
-    row_id = "image-row-" + String(row_to_show);
-    var row = document.getElementById(row_id);
-    row.classList.toggle("d-none");
-    if(row_to_show==num_rows){
-        document.getElementById("show_more_button").classList.toggle("d-none");
+    const mediaRows = document.querySelectorAll('.mediaRow');
+    const lastRowShown = mediaRows[mediaRows.length-1];
+    const firstIndex = 4*rowsShowing+1;
+    const lastIndex = 4*rowsShowing+5;
+
+    for(let i = firstIndex; i < lastIndex; i++){
+        let img = document.createElement('img');
+        img.src = `files/${i}.jpeg`;
+        mediaRow.append(img);
     }
-});
 
-images = document.querySelectorAll('img.img-fluid');
-images.forEach(function(e){
-    e.addEventListener('click', function(f){
-        document.getElementById("jumbo-image").src = e.src;
-        document.getElementById("clicked-image-jumbo").classList.toggle("d-md-inline");
-    });
-});
+    lastRowShown.insertAdjacentElement('afterend', mediaRow);
 
-var close_button = document.getElementById("close-jumbo-button");
-close_button.addEventListener("click", function(e){
-    document.getElementById("clicked-image-jumbo").classList.toggle("d-md-inline");
-});
-
-var jumbo_image = document.getElementById("jumbo-image");
-var chev_right = document.getElementById("chev-right");
-var chev_left = document.getElementById("chev-left");
-chev_right.addEventListener("click", function(e){
-    var jumbo_image_src = document.getElementById("jumbo-image").src;
-    var jumbo_image_number = Number(jumbo_image_src.slice(-7,-5));
-    var new_image_number = jumbo_image_number + 1;
-    var new_image_src;
-    if(new_image_number < 10){
-        new_image_src = "files/0" + String(new_image_number) + ".jpeg"
-    } else if(jumbo_image_number == num_pics){
-        new_image_src = jumbo_image_src;
+    if(rowsShowing == 16){
+        showMoreButton.remove();
     } else {
-        new_image_src = "files/" + String(new_image_number) + ".jpeg"
+        rowsShowing++;    
     }
-    jumbo_image.src = new_image_src;
-});
+}
 
-chev_left.addEventListener("click", function(e){
-    var jumbo_image_src = document.getElementById("jumbo-image").src;
-    var jumbo_image_number = Number(jumbo_image_src.slice(-7,-5));
-    var new_image_number = jumbo_image_number - 1;
-    var new_image_src;
-    if(jumbo_image_number == 1){
-        new_image_src = jumbo_image_src;
-    } else if(new_image_number < 10){
-        new_image_src = "files/0" + String(new_image_number) + ".jpeg"
-    } else {
-        new_image_src = "files/" + String(new_image_number) + ".jpeg"
-    }
-    jumbo_image.src = new_image_src;
-});
+let rowsShowing = 3;
+
+const showMoreButton = document.querySelector('#showMoreButton');
+showMoreButton.addEventListener('click', showNextRow);
