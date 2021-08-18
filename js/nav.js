a52f390ba2642eef723914022c1ac45c3f9df647
samharshe <samharshe29@gmail.com>
2021-08-18 10:54:40 -0400

    cleaned everything up

---
+++ js/nav.js
@@ -0,0 +1,7 @@
+const navDisplayToggle = document.querySelector("#navDisplayToggle");
+const navItems = document.querySelectorAll('.navItem');
+navDisplayToggle.addEventListener('click', function(){
+    navItems.forEach(navItem => {
+        navItem.classList.toggle('hidden');
+    });
+});
