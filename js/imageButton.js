c08493715a67d988dfdc11083b93363781c6c9bf
samharshe <samharshe29@gmail.com>
2021-12-15 10:31:01 -0500

    changed imageButton so that which images appear is random

--- js/imageButton.js
+++ js/imageButton.js
@@ -1,18 +1,54 @@
-const addImages = function(){
+const addImagesOnPageLoad = function(){
+    const initialImages = document.querySelectorAll('img')
+    initialImages.forEach(image => image.src = getImageName(nums.pop()))
+}
+
+const addImagesOnButtonClick = function(){
     for(let i = 0; i < 4; i++){
+        const images = document.querySelectorAll('img')
+        lastImg = images[images.length - 1]
+
         let imgToAdd = document.createElement('img');
-        imgToAdd.src = `files/${numImagesShowing+1}.jpeg`;
-        let lastImg = document.querySelector(`img[src="files/${numImagesShowing}.jpeg"]`)
+        imgToAdd.src = getImageName(nums.pop())
+
         lastImg.insertAdjacentElement('afterend', imgToAdd)
         numImagesShowing++;
     }
 
-    if(numImagesShowing == 64){
+    if(nums.length == 0){
         morePhotosButton.remove();
     }
 }
 
+const getImageName = function(n){
+    if(n < 10){
+        return `files/0${n}.jpeg`
+    } else {
+        return `files/${n}.jpeg`
+    }
+}
+
+const shuffle = function(array){
+    let l = array.length, t, i
+
+    while(l){
+        i = Math.floor(Math.random() * l)
+        l--
+
+        t = array[l]
+        array[l] = array[i]
+        array[i] = t
+    }
+
+    return array
+}
+
+let nums = shuffle(Array.from({length: 68}, (_, i) => i + 1))
+console.log(nums)
+
 let numImagesShowing = 12;
 
-const morePhotosButton = document.querySelector('#morePhotosButton');
-morePhotosButton.addEventListener('click', addImages);
+const morePhotosButton = document.querySelector('#morePhotosButton')
+morePhotosButton.addEventListener('click', addImagesOnButtonClick)
+
+addImagesOnPageLoad()
