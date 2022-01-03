7afc2b3718936bea4baddf2b21b5aa577658a3d0
samharshe <samharshe29@gmail.com>
2022-01-03 11:50:48 -0500

    added footer to essay page

--- js/nav.js
+++ js/nav.js
@@ -2,6 +2,6 @@ const navDisplayToggle = document.querySelector("#navDisplayToggle");
 const navItems = document.querySelectorAll('.navItem');
 navDisplayToggle.addEventListener('click', function(){
     navItems.forEach(navItem => {
-        navItem.classList.toggle('hidden');
+        navItem.classList.toggle('revealed');
     });
 });
